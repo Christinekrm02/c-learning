794454a8fc0f773acd39eaf0e3ea4659df6b1574
Christine Maynard <christinekrm02@gmail.com>
2021-05-12 19:29:19 -0400

    Practice for loop

---
+++ foundations/conditionals/loops/for-loop.cpp
@@ -0,0 +1,8 @@
+//useful when you know the number of times you want to check criteria
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i < 10; i++){
+cout << "I will not throw paper planes \n";
+    }
+}
