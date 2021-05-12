8f3dbd7beeee650609431c444f55551416459a19
Christine Maynard <christinekrm02@gmail.com>
2021-05-12 19:08:56 -0400

    Implement two lists the first list is a set of integers less than 10 the second list is the squared value of each integer use while loop

---
+++ foundations/conditionals/loops/square.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 0;
+    int square = i * i;
+
+    while(i < 10 ){
+        cout << i << " " << square << "\n";
+        i++;
+    }
+}
